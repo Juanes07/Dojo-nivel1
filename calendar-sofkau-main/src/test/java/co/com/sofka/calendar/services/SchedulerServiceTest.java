1d71cd6893ee131ce29ecc5e9bf94b72aaf35726
Camila Morales <camila.moralesg21@gmail.com>
2022-05-27 11:41:54 -0500

    ToDo Solucionado - Test - programNoFound
    Se cumplen todo los ToDos del test programNoFound

--- calendar-sofkau-main/src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
+++ calendar-sofkau-main/src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
@@ -120,11 +120,19 @@ class SchedulerServiceTest {
         Mockito.when(repository.findById(programId)).thenReturn(Mono.empty());
 
         //TODO: hacer de otro modo
-        var exception = Assertions.assertThrows(RuntimeException.class, () -> {
+       /* var exception = Assertions.assertThrows(RuntimeException.class, () -> {
             schedulerService.generateCalendar(programId, startDate);//TODO: hacer una subscripción de el servicio reactivo
 
-        });
-        Assertions.assertEquals("El programa academnico no existe", exception.getMessage());//TODO: hacer de otro modo
+        });*/
+
+        Flux<ProgramDate> response = schedulerService.generateCalendar(programId, startDate);
+        response.subscribe();
+
+        StepVerifier
+                .create(response)
+                .expectErrorMessage("El programa academnico no existe")
+                .verify();
+        //Assertions.assertEquals("El programa academnico no existe", exception.getMessage());//TODO: hacer de otro modo
         Mockito.verify(repository).findById(programId);
 
     }
